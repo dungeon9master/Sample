b9a7a9aed6483f5000b3a5f0b59ecfcf4e1d456a
dungeon9master <mayankkhanna038@gmail.com>
2019-07-29 18:50:51 +0530

    first

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+	root 'application#home'
+	resources :users
+	get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'
+	get 'login',to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout',to: 'sessions#destroy'
+
+end
